d000be3031235da45e2a78c468754fa8695f00c3
Jessy Yameogo <jessy.yameogo@gmail.com>
2016-02-28 12:37:26 -0500

    SQLAlchemy update

--- project/models.py
+++ project/models.py
@@ -1,7 +1,19 @@
 from view import db
 
-def sum(a,b):
-    return a+b
+class Task(db.Model):
+    __tablename__ = "tasks"
 
-y = sum(x,y)
-h
+    task_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=True)
+    due_date = db.Colum(db.Date,nullable=True)
+    priority = db.Column(db.Integer,nullable=True)
+    status = db.Column(db.Integer)
+
+    def __init__(self,name,due_date,priority,status):
+        self.name = name
+        self.due_date = due_date
+        self.priority = priority
+        self.status = status
+
+    def __repr__(self):
+        return '<name {0}>'.format(self.name)
